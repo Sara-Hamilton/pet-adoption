ba0c1f0617e77cd8d45f471a010c08439b8480c1
Stephanie Faber and Sara Hamilton <stephanie@>
2018-01-29 10:19:34 -0800

    add pet object constructor

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function Pet() {
+  this.name = name;
+  this.age = age;
+  this.type = type;
+  this.size = size;
+  this.image = image;
+  this.description = description;
+  this.heart = false;
+  this.adopted = false;
+}
